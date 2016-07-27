98fe63c2c998717b880ee973d4f6e7acc5531b9c
Lucy Fang <lucyfang2@gmail.com>
2016-07-27 14:48:44 +0100

    Added node express and app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var root = __dirname;
+var path = require('path');
+
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(root + '/public/example_simple_exportwav.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
