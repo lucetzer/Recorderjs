5b4b5d42b28ad5404e0dd1e512881a0e7487b4a4
Lucy Fang <lucyfang2@gmail.com>
2016-07-27 15:05:23 +0100

    added dynamic port assignment

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(root + '/public/example_simple_exportwav.html'));
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
